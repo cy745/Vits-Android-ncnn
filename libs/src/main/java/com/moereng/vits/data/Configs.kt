804c7b676266800009fe8d41bd3ddba63c0db383
lalilu <1248393538@qq.com>
2023-01-10 20:46:50 +0800

    关键组件拆分

---
+++ libs/src/main/java/com/moereng/vits/data/Configs.kt
@@ -0,0 +1,5 @@
+package com.moereng.vits.data
+
+data class Configs(val data: MData, val symbols: List<String>, val speakers: List<String>) {
+    data class MData(val text_cleaners: List<String>, val n_speakers: Int)
+}
